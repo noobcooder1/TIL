4cfa93a46027ae5680168aa92ecc4c035e5c4ce5
gyubly <eoghkwo800@gmail.com>
2025-09-27 23:58:34 +0900

    Add initial Java study project files with examples
    - Included basic Java example files covering various concepts such as loops, methods, and data structures.
    - Added `.gitignore` to exclude IntelliJ IDEA-specific files and directories.
    - Set up initial IntelliJ IDEA project configuration files.

---
+++ java study/src/JustPractice/RandomQuiz.java
@@ -0,0 +1,12 @@
+package JustPractice;
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomQuiz {
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+
+
+        s1.nextLine();
+    }
+}

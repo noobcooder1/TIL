db6d492a4857aa28197fbe72aa02b8b876ace1ad
gyubly <eoghkwo800@gmail.com>
2025-10-01 16:24:11 +0900

    오늘 배운 파이썬

---
+++ python study/5장/연습문제4.py
@@ -0,0 +1,16 @@
+score = int(input("성적을 입력하세요."))
+
+if score >= 90 :
+    print("A학점")
+
+elif score >= 80 :
+    print("B학점")
+
+elif score >= 70 :
+    print("B학점")
+
+elif score >= 60 :
+    print("B학점")
+
+else :
+    print("F입니다. 분발하세요.")

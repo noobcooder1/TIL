af7665a1ac0ef1357c060f3e0574a0edb8242d6f
gyubly <eoghkwo800@gmail.com>
2025-10-02 11:49:32 +0900

    불필요한 설정파일들을 커밋에서 제외시키기위한 깃이그노어 커밋

--- flutter study/flutter app/app1/lib/exam.dart
+++ flutter study/flutter app/app1/lib/exam.dart
@@ -11,10 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My first flutter app',
       theme: ThemeData(
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );

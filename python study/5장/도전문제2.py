fc9ce5ba002532977ff02c3b04046b2c517d59c6
gyubly <eoghkwo800@gmail.com>
2025-10-08 16:40:10 +0900

    오늘 제출할 파이썬 도전과제

--- python study/5장/도전문제2.py
+++ python study/5장/도전문제2.py
@@ -1,1 +1,16 @@
 # [실습2] P.143 주사위던지기 게임 완성하기
+import random 
+
+print("주사위 게임에 오신 것을 환영합니다.")
+
+sum = 0
+
+roll = int(input("주사위를 굴릴 횟수를 입력하세요 :"))
+
+for i in range(roll):
+    dice = random.randint(1,6)
+    print(f"{i+1}번째 주사위의 값은 {dice}입니다.")
+    sum += dice
+
+print(f"총 주사위의 합은 {sum}입니다.")
+print("게임이 종료되었습니다. 이용해주셔서 감사합니다.")
--- python study/5장/도전문제3.py
+++ python study/5장/도전문제3.py
@@ -1,2 +1,14 @@
 # [실습3] 연습문제 4번 : if – elif- else 구문 사용
 
+score = int(input("점수를 입력하세요: "))
+
+if score >= 90:
+    print("A학점 입니다.")
+elif score >= 80:
+    print("B학점 입니다.")
+elif score >= 70:
+    print("C학점 입니다.")
+elif score >= 60:
+    print("D학점 입니다.")
+else:
+    print("F학점 입니다.")

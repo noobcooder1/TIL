0dcebadfec0989e4651c5e1fabe60d458b1669c8
gyubly <eoghkwo800@gmail.com>
2025-10-15 14:43:24 +0900

    1교시종료후커밋

---
+++ python study/6장/사각형그리기.py
@@ -0,0 +1,32 @@
+import turtle as t
+
+Colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "cyan", "magenta", "lime", "teal", "lavender"]
+size = int(input("사각형의 한 변의 길이를 입력하세요: "))
+gak = int(input("사각형을 회전시킬 각도를 입력하세요: "))
+
+def drawSquare(size): 
+    t.forward(size)
+    t.left(90)   # 또는 t.right(90)
+    t.forward(size)
+    t.left(90)   # 또는 t.right(90)
+    t.forward(size)
+    t.left(90)   # 또는 t.right(90)
+    t.forward(size)
+    t.left(90)   # 또는 t.right(90)
+    t.left(gak)
+
+t.shape("turtle")
+t.pensize(6)
+
+t.penup()
+t.goto(0, 0)
+t.pendown()
+
+for i in range(360/gak): 
+    t.pencolor(Colors[i % len(Colors)])
+    drawSquare(size)
+
+
+t.done()
+    
+

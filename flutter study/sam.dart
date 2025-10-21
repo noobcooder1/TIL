2520275b3baffa8bc7bd7d426b9e878695ad4f6d
gyubly <eoghkwo800@gmail.com>
2025-10-21 11:21:03 +0900

    flutter second_app 추가

--- flutter study/sam.dart
+++ flutter study/sam.dart
@@ -1,11 +1,3 @@
 void main() {
-var num2 = <int>[1, 2, 3];
-printList(num2);
-
-}
-
-void printList(nums) {
-  for (var i = 0; i < nums.length; i++) {
-    print(nums[i]);
-  }
+  print('Hello, Flutter Study!');
 }
